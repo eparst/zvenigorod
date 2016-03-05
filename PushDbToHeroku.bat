53f95e4d50ebab9b05e930ab8cbd1be3b710ac55
eparst q <eparst q>
2016-03-06 00:30:01 +0300

    stage

--- PushDbToHeroku.bat
+++ PushDbToHeroku.bat
@@ -11,4 +11,10 @@ curl -o b013.dump `heroku pg:backups public-url`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d zvenigorod C:\j\2015_10\zvenigorod\1.dump
 
 to openshift:
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U adminhpcaky5 -d oleg C:\j\2015_10\tmp\oleg.dump
+через port-forward: pg_restore --verbose --clean --no-acl --no-owner -h localhost -U adminhpcaky5 -d oleg C:\j\2015_10\tmp\oleg.dump
+через ssh вход:
+curl --user rus-f1@ya.ru:gtxty.irf1 -o oleg.dump https://webdav.yandex.ru/oleg.dump
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U adminhpcaky5 -d oleg oleg.dump
+
+psql -U adminhpcaky5  -d oleg -W -f oleg.dump
+vYX4xtwr2kzw
